98b772655018993be0bf973ee55c88f87831adff
Reagan D Duecker <dueckerr@gmail.com>
2019-02-13 15:10:38 -0700

    Firebase saving college info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ var collegeScoreCardApiKey = "&api_key=h9FV3Fo58pfjzQYWYFNBjwf8imebCZj2t18pebBA"
 var collegeNameInput = "";
 var collegeScoreCardURL = "https://api.data.gov/ed/collegescorecard/v1/";
 
+
 $("#submit-search").on("click", function(){
     event.preventDefault();
     collegeNameInput = $("#college-name-input").val().trim();
@@ -11,7 +12,7 @@ $("#submit-search").on("click", function(){
         url: `https://api.data.gov/ed/collegescorecard/v1/${collegeScoreCardSchoolName}${collegeScoreCardApiKey}`,
             method: "GET"
           }).then(function(response) {
-              
+              showResults(response)
             $("#collegeName").html(`<h1>${response.results[0].school.name}</h1>`);
             $("#location").html(`<b>Location:</b> ${response.results[0].school.city}, ${response.results[0].school.state}`)
             $("#tuitionInstate").html(`<b>In state cost:</b> $${response.results[0].latest.cost.tuition.in_state}`);
@@ -19,74 +20,53 @@ $("#submit-search").on("click", function(){
             $("#acceptanceRate").html(`<b>Acceptance Rate:</b> ${response.results[0].latest.admissions.admission_rate.overall}%`);
             // $("#SixYearIncome").html(`<b>Median income after six years: </b>$${respsone.results[0].latest.earnings..median}`)
             console.log(response);
-            console.log(`${response.results[0].school.city}`)
+
+            // Saves search variables for firebase to capture when save button is clicked
+
+            newCollege = `${response.results[0].school.name}`
+            newCity = `${response.results[0].school.city}`
+            newtuition_Instate = `${response.results[0].latest.cost.tuition.in_state}`
+            newtuition_Outstate = `${response.results[0].latest.cost.tuition.out_of_state}`
+            newacceptRate  = `${response.results[0].latest.admissions.admission_rate.overall}`
+            console.log(newCity);
+            newstate = `${response.results[0].school.state}`
           });
-
-    console.log("button works");
-
+            
+          console.log("button works");
 });
 
-var config = {
-  apiKey: "AIzaSyCyWIXZHywteI0KWaauMi7sjWeyL_DDDcc",
-  authDomain: "college-finder-f2730.firebaseapp.com",
-  databaseURL: "https://college-finder-f2730.firebaseio.com",
-  projectId: "college-finder-f2730",
-  storageBucket: "college-finder-f2730.appspot.com",
-  messagingSenderId: "109608559117"
-
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-// add new college info
-$("#add-college-btn").on("click", function(event) {
-  event.preventDefault();
-
-  // pulls user input/new college info
-  var newCollege = $("#collegeName").val().trim();
-  var newCity = $("#location").val().trim();
-  var newtuition_Instate = $("#tuitonInstate");
-  var newtuition_Outstate = $("#tuitionOutstate");
-  var newacceptRate = $("#acceptanceRate");
-
-  var newCollegeinfo = {
-
-      college: newCollege,
-      city: newCity,
-      tuition_Instate: newtuition_Instate,
-      tuition_Outstate: newtuition_Outstate,
-      acceptRate: newacceptRate,
-      // income: newIncome,
+    var config = {
+      apiKey: "AIzaSyCyWIXZHywteI0KWaauMi7sjWeyL_DDDcc",
+      authDomain: "college-finder-f2730.firebaseapp.com",
+      databaseURL: "https://college-finder-f2730.firebaseio.com",
+      projectId: "college-finder-f2730",
+      storageBucket: "college-finder-f2730.appspot.com",
+      messagingSenderId: "109608559117"
+      
   };
 
-  // push's new info to database
-  database.ref().push(newCollegeinfo);
+    firebase.initializeApp(config);
+    
+    var database = firebase.database();
+    
+    // Button that pushes var for firebase
 
-});
+    function showResults(data) {$("#add-college-btn").on("click", function(event) {
+          event.preventDefault();
+          console.log(newCity);
 
-
-database.ref().on("child_added", function(childSnapshot) {
-
-  var newCollege = childSnapshot.val().college;
-  var newCity = childSnapshot.val().city;
-  var newtuition_Instate = childSnapshot.val().tuition_Instate;
-  var newtuition_Outstate = childSnapshot.val().tuition_Outstate;
-  var newacceptRate = childSnapshot.val().acceptRate;
-  // var newIncome = childSnapshot.val().income;ß
-
-  // new row for college
-  var savedCollege= $("<tr>").append(
-
-      $("<td>").text(newCollege),
-      $("<td>").text(newCity),
-      $("<td>").text(newtuition_Instate),
-      $("<td>").text(newtuition_Outstate),        
-      $("<td>").text(newacceptRate),
-      // $("<td>").text(newIncome),
-
-  );
-
-  $("#collegeInfo-table > tbody").append(savedCollege);
-
-});
+          var newCollegeinfo = {
+            
+            college: newCollege,
+            city: newCity + "," + newstate,
+            tuition_Instate: newtuition_Instate,
+            tuition_Outstate: newtuition_Outstate,
+            acceptRate: newacceptRate,
+            // income: newIncome,
+          };
+          
+          // push's new info to database
+          
+          database.ref().push(newCollegeinfo);
+       });
+    }
